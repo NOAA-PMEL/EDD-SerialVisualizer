efa28eec0b03d62d44c88c40ed0febdccb77f5e3
MCasari-PMEL <matthew.casari@noaa.gov>
2017-05-03 07:42:54 -0700

    framework for servis gui

--- SerVis/SerVis.py
+++ SerVis/SerVis.py
@@ -13,12 +13,15 @@ import pyqtgraph.console
 import numpy as np
 
 from serial_console_widget import SerialConsoleWidget
+from serial_receive_widget import SerialReceiveParser
 from serial_widget import SerialWidget
 from clock_widget import DigitalClock
 from plot_raw import RawPlotWidget
 
 import sys
-import serial 
+import serial
+
+import time
 
 
 class MainWindow(QtGui.QMainWindow):
@@ -52,30 +55,32 @@ class MainWindow(QtGui.QMainWindow):
         ## fill the entire dock area and obey the limits of their internal widgets.
         self.area = DockArea()
 
-        self.d0 = Dock("Description",size=(50,25))
-        self.d1 = Dock("Serial Port",size=(60,200))
-        self.d3 = Dock("Date/Time",size=(60,25))
-        self.d4 = Dock("PMEL Logo",size=(60,60))
-        self.d5 = Dock("Data Plot",size=(550,550))
-        self.d5a = Dock("Data",size=(200,50))
-        self.d6 = Dock("Serial Console",size=(200,300))
+        self.dock0 = Dock("Description",size=(50,25))
+        self.dock1 = Dock("Serial Port",size=(60,200))
+        self.dock2 = Dock("Date/Time",size=(60,25))
+        self.dock3 = Dock("PMEL Logo",size=(60,60))
+        self.dock5 = Dock("Data Plot",size=(550,550))
+        self.dock6 = Dock("Data",size=(200,50))
+        self.dock7 = Dock("Serial Console",size=(200,300))
+        self.dock8 = Dock("Serial Receive",size=(200,300))
         
 
-        self.area.addDock(self.d0,'left')
-        self.area.addDock(self.d1,'bottom',self.d0)
-        self.area.addDock(self.d5,'right',self.d1)
+        self.area.addDock(self.dock0,'left')
+        self.area.addDock(self.dock1,'bottom',self.dock0)
+        self.area.addDock(self.dock5,'right',self.dock1)
 
-        self.area.addDock(self.d5a,'bottom',self.d5)
-        self.area.addDock(self.d6,'bottom',self.d5a)
-        self.area.addDock(self.d3,'bottom',self.d1)
-        self.area.addDock(self.d4,'bottom',self.d3)
+        self.area.addDock(self.dock6,'bottom',self.dock5)
+        self.area.addDock(self.dock7,'bottom',self.dock6)
+        self.area.addDock(self.dock2,'bottom',self.dock1)
+        self.area.addDock(self.dock3,'bottom',self.dock2)
+        self.area.addDock(self.dock8,'right',self.dock3)
         
         
         ## Add widgets into each dock
         ## GUI Description 
-        self.w0 = pg.LayoutWidget()
-        self.w0.setAutoFillBackground(True)
-        palette = self.w0.palette()
+        self.w_title = pg.LayoutWidget()
+        self.w_title.setAutoFillBackground(True)
+        palette = self.w_title.palette()
         palette.setColor(QtGui.QPalette.Window,QtGui.QColor('black'))
         label = QtGui.QLabel(""" SerVis  (Serial Visualizer) """)
         font = QtGui.QFont( "Helvetica", 18)
@@ -83,65 +88,55 @@ class MainWindow(QtGui.QMainWindow):
         label.setFont(font)
         label.setStyleSheet("color: black")
         label.setAlignment(QtCore.Qt.AlignCenter)
-        self.w0.addWidget(label, row=0, col=0)
-        self.d0.addWidget(self.w0)
-        self.d0.hideTitleBar()
+        self.w_title.addWidget(label, row=0, col=0)
+        self.dock0.addWidget(self.w_title)
+        self.dock0.hideTitleBar()
 
-        #self.w0.setConfigOption('background','w')
-        #self.w0.setConfigOption('foreground','k')
+        #self.w_title.setConfigOption('background','w')
+        #self.w_title.setConfigOption('foreground','k')
 
 
 
         ## SERIAL PORT Widget
-        self.w1 = SerialWidget()
-        self.d1.addWidget(self.w1)
+        self.w_port = SerialWidget()
+        self.dock1.addWidget(self.w_port)
 
         ## UTC CLOCK Widget
-        self.w3 = DigitalClock()
-        self.d3.addWidget(self.w3)
+        self.w_clock = DigitalClock()
+        self.dock2.addWidget(self.w_clock)
 
         ## MEATBALL
-        self.w4 = pg.LayoutWidget()
+        self.w_pmel = pg.LayoutWidget()
         pic = QtGui.QLabel()
         pixmap = QtGui.QPixmap("PMEL.png")
         pic.setPixmap(pixmap)
         pic.setScaledContents(True)
         pic.setMaximumSize(150,150)
-        self.w4.addWidget(pic, row=0, col=0)
-        self.d4.addWidget(self.w4)
-        self.d4.hideTitleBar()
+        self.w_pmel.addWidget(pic, row=0, col=0)
+        self.dock3.addWidget(self.w_pmel)
+        self.dock3.hideTitleBar()
 
         ## DATA PLOT Widget
-        self.w5 = RawPlotWidget()
-        self.d5.addWidget(self.w5)
+        self.w_plot = RawPlotWidget()
+        self.dock5.addWidget(self.w_plot)
 
         ## CURRENT Data Widget
-        #self.w5a = CurrentDataWidget()
-        #self.d5a.addWidget(self.w5a)
+        #self.w_plota = CurrentDataWidget()
+        #self.dock6.addWidget(self.w_plota)
 
         ## SERIAL CONSOLE Widget
-        self.w6 = SerialConsoleWidget()
-        self.d6.addWidget(self.w6)
-##        self.w6 = pg.LayoutWidget()
-##        self.label6 = QtGui.QLabel("""  SERIAL CONSOLE """)
-##        self.label6.setAlignment(QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom)
-##        self.w6.addWidget(self.label6, row=0, col=0)
-##        self.d6.addWidget(self.w6)
-##        self.d6.setMaximumHeight(250)
+        self.w_serial = SerialConsoleWidget()
+        self.dock7.addWidget(self.w_serial)
 
-        ## SYSTEM SETTING Parameters
-        #self.w7 = SystemSettingWidget()
-        #self.d7.addWidget(self.w7)
-
-        ## SENSOR TYPE Widget
-        #self.w8 = ImageWidget()
-        #self.d8.addWidget(self.w8)
+        ## SERIAL RECEIVE WIDGET
+        self.w_receive = SerialReceiveParser()
+        self.dock8.addWidget(self.w_receive)
 
 
         ## System Event Timer
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.update)
-        self.timer.start(100)
+        self.timer.start(100)   
 
         self.setCentralWidget(self.area)
 
@@ -163,16 +158,16 @@ class MainWindow(QtGui.QMainWindow):
 
     
         ## Update the Serial Buffer (read the port)
-        self.w1.update_serial_buffer()
+        self.w_port.update_serial_buffer()
         
         ## If there is actually data, parse it and append it to the string buffer for the serial console
-        if(len(self.w1.data) > 0):
+        if(len(self.w_port.data) > 0):
             try:
                 ## Parse the string
-                self.parse_serial_data(self.w1.data)
+                self.parse_serial_data(self.w_port.data)
 
                 ## Append the string to the running buffer
-                self.dataStr.append(self.w1.data)  
+                self.dataStr.append(self.w_port.data)  
             except:
                 pass
         try:
@@ -187,171 +182,7 @@ class MainWindow(QtGui.QMainWindow):
 
         
 
-    def parse_serial_data(self,buf):
-        """ Parse the data on the serial port
-
-        Determines which method of parsing should be accomplished based on current sensor settings
-
-        Possible modes:
-          Streaming - Raw Data
-          Streaming - Instantaneous Data
-          Streaming - Statistical Data
-
-        Args:
-          buf: Current Serial Port Buffer string
-
-        Returns:
-          There are no returns for this function
-
-        Raises:
-          None
-        """
-        if(self.stream_mode == 'Raw'):
-            self.parse_raw(buf)
-        if(self.stream_mode == 'Inst'):
-            self.parse_instantaneous(buf)
-        if(self.stream_mode == 'Stats'):
-            print("In Stats")
-            self.parse_statistical(buf)
-
-    def parse_instantaneous(self,buf):
-        """ Parse instantaneous mode data
-        Takes the serial port buffer string and parses it based on instantaneous
-        mode for the following variables:
-
-          %s - Timestamp (YYYY/MM/DD HH:MM:SS)
-          %f - Net Radiation (W/m^2)
-          %f - Dome Temperature (Deg C)    <-- Ignored in Short Wave Sensor
-          %f - Case Temperature (Deg C)
-          %f - Downwell Radiation (Compensated)
-
-        Args:
-          buf: Current serial port buffer string to be parsed
-
-        Returns:
-          There are no returns for this function
-
-        Raises:
-          ValueError: An error occured splitting the buffer
-
-        """
-        try:
-            buf = buf.split('\r\n')
-            values = buf[0].split(' ')
-        except ValueError:
-            pass
-        pass
-
-    def parse_statistical(self,buf):
-        """ Parse statistical mode data
-        Takes the serial port buffer string and parses it based on raw mode
-        for the following variables:
-
-          %s - Timestamp (YYYY/MM/DD HH:MM:SS)
-          %d - Thermopile voltage (Raw Counts)
-          %d - Dome Thermistor Voltage (Raw Counts)
-          %d - Case Thermistor Voltage (Raw Counts)
-
-        Args:
-          buf: Current serial port buffer string to be parsed
-        Returns:
-          There are no returns for this function
-        Raises:
-          Value Error: An error occured splitting the buffer
-
-        """
-        try:
-            buf = buf.split('\r\n')
-            values = buf[0].split(' ')
-        except ValueError:
-            pass
-
-        for val in values:
-            if val[0] == 'R':
-                rad = float(val[1:])
-            if val[0] == 'S':
-                std = float(val[1:])
-            if val[0] == 'D':
-                dome = float(val[1:])
-            if val[0] == 'C':
-                case = float(val[1:])
-
-
-        print(dome)
-        print(case)
-        self.w5.addData(rad,std,dome,case)
-
-    def parse_raw(self,buf):
-        """ Parse raw mode data
-        Takes the serial port buffer string and parses it based on raw mode
-        for the following variables:
-
-          %s - Timestamp (YYYY/MM/DD HH:MM:SS)
-          %d - Thermopile voltage (Raw Counts)
-          %d - Dome Thermistor Voltage (Raw Counts)
-          %d - Case Thermistor Voltage (Raw Counts)
-
-        Args:
-          buf: Current serial port buffer string to be parsed
-        Returns:
-          There are no returns for this function
-        Raises:
-          Value Error: An error occured splitting the buffer
-          
-        """
-        try:
-            buf = buf.split('\r\n')
-            values = buf[0].split(' ')
-        except ValueError:
-            pass
-
-        
-        for val in values:
-            if val[0] == 'R':
-                raw = float(val[1:])
-            if val[0] == 'D':
-                dome = float(val[1:])
-            if val[0] == 'C':
-                case = float(val[1:])
-
-
-        self.w5.addData(raw,dome,case)
-##        self.w5a.SetRnet(raw)
-##        self.w5a.SetDome(dome)
-##        self.w5a.SetCase(case)
-##        
-##        self.data1.append(np.array([raw]))
-##        self.data2.append(np.array([dome]))
-##        self.data3.append(np.array([case]))
-##        self.data4.append(np.array([0]))
-##        
-##
-##        self.count += 1
-##
-##        self.plot_raw()
     
-        pass
-
-   
-
-    def plot_raw(self):
-        """ Plot the raw data values
-        Takes the current buffer of raw values and plots them.
-        """
-
-        if(len(self.data1)>=100):
-            self.data1.pop(0)
-            self.data2.pop(0)
-            self.data3.pop(0)
-            self.data4.pop(0)
-
-        self.curve1.setData(np.hstack(self.data1))
-        self.curve2.setData(np.hstack(self.data2))
-        self.curve3.setData(np.hstack(self.data3))
-        self.curve4.setData(np.hstack(self.data4))
-        
-        pass
-
     def plot_inst(self):
         pass
 
